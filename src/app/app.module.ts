0f20eeccce2c24a59b55972b720e16747e6d5065
willow <jurate.baltrukenaite@gmail.com>
2020-12-12 23:00:26 +0200

    initial setup

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,25 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { NoopAnimationsModule } from '@angular/platform-browser/animations';
+import { MatToolbarModule } from '@angular/material';
+import { SignInComponent } from './sign-in/sign-in.component';
+
+const materialModules = [MatToolbarModule]
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    SignInComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    NoopAnimationsModule,
+    materialModules
+
   ],
   providers: [],
   bootstrap: [AppComponent]
