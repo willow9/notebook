ec2bad8c980b7bad8931f37712c063e51a30220b
willow <jurate.baltrukenaite@gmail.com>
2020-12-13 16:28:08 +0200

    Some changes in register component 2

--- src/app/navbar/navbar.component.ts
+++ src/app/navbar/navbar.component.ts
@@ -6,7 +6,7 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./navbar.component.css']
 })
 export class NavbarComponent implements OnInit {
-  logedInUser = true;
+  logedInUser = false;
 
   constructor() { }
 
--- src/app/register/register.component.ts
+++ src/app/register/register.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-register',
@@ -7,7 +8,28 @@ import { Component, OnInit } from '@angular/core';
 })
 export class RegisterComponent implements OnInit {
 
-  constructor() { }
+
+  rForm: FormGroup;
+  credentials: any;
+  email: string = '';
+  password: string = '';
+
+  constructor(private fb: FormBuilder) {
+    this.rForm = fb.group({
+      'email': [null, [Validators.required, Validators.email]],
+      'password': [null, Validators.compose([Validators.required, Validators.minLength(6)])]
+    })
+  }
+
+  addCredentials(credentials) {
+    this.email = credentials.email;
+    this.password = credentials.password
+  }
+  registerUser() {
+    console.log(this.rForm);
+
+  }
+
 
   ngOnInit() {
   }
